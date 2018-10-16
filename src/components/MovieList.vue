f7edcdc81976a1e5c0e2a161965bf649f2a530ae
Jorge Fernandes <jorge.fernandes@timewax.com>
2018-10-16 12:33:59 +0100

    Lesson 082

---
+++ src/components/MovieList.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="movie-list">
+		<div class="movie" v-for="movie in filteredMovies">{{ movie.title }}</div>
+	</div>
+</template>
+<script>
+	import genres from '../util/genres';
+
+	export default {
+		data() {
+			return {
+				movies: [
+					{ title: 'Pulp Fiction', genre: genres.SCIFI },
+					{ title: 'Home Alone', genre: genres.COMEDY },
+					{ title: 'Austin Powers', genre: genres.COMEDY }
+				]
+			};
+		},
+		props: [
+			'genre',
+			'time'
+		],
+		methods: {
+			moviesPassGenreFilter(movie) {
+				if(!this.genre.length) {
+					return true;
+				} else {
+					return this.genre.find(genre => movie.genre === genre);
+				}
+			}
+		},
+		computed: {
+			filteredMovies() {
+				return this.movies.filter(this.moviesPassGenreFilter);
+			}
+		}
+	}
+</script>
