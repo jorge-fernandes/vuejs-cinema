0b1739fc12b065565be6ad67ee973bde466dc0cd
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-01-10 15:42:26 +0000

    Lesson 085

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,24 +1,16 @@
 <template>
 	<div id="movie-list">
-		<div class="movie" v-for="movie in filteredMovies">{{ movie.title }}</div>
+		<div class="movie" v-for="movie in filteredMovies">{{ movie.movie.Title }}</div>
 	</div>
 </template>
 <script>
 	import genres from '../util/genres';
 
 	export default {
-		data() {
-			return {
-				movies: [
-					{ title: 'Pulp Fiction', genre: genres.SCIFI },
-					{ title: 'Home Alone', genre: genres.COMEDY },
-					{ title: 'Austin Powers', genre: genres.COMEDY }
-				]
-			};
-		},
 		props: [
 			'genre',
-			'time'
+			'time',
+			'movies'
 		],
 		methods: {
 			moviesPassGenreFilter(movie) {
